1527b274bab30130c9eff18b9c3604a2fc764239
ArowuTest <fsanus20111@gmail.com>
2025-04-17 00:24:36 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
-import '../styles/globals.css';
 import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../components/GlobalStyles';
 import theme from '../theme';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Component {...pageProps} />
     </ThemeProvider>
   );
