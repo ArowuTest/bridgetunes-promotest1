872596f5092497d12887764ce4d90450bd862c86
ArowuTest <fsanus20111@gmail.com>
2025-04-22 17:08:57 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,13 @@
-// pages/_app.js
+import '../styles/globals.css';
 import { ThemeProvider } from 'styled-components';
 import theme from '../theme';
-
-// Import GlobalStyles only if it exists
-let GlobalStyles = () => null;
-try {
-  GlobalStyles = require('../components/GlobalStyles').default;
-} catch (e) {
-  console.warn('GlobalStyles not found, using fallback');
-}
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
       <Component {...pageProps} />
     </ThemeProvider>
   );
 }
 
 export default MyApp;
-
-
